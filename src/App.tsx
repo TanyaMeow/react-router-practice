16f8e53e46494bb95b614d073156b7222eb26f23
Татьяна Хонякина <hanakinat@gmail.com>
2023-10-25 10:27:16 +0300

    Импорт всех компонентов и роутинг

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,38 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {NavLink, redirect, Route, Routes} from "react-router-dom";
+import {ContainerFacts} from "./components/ContainerFacts";
+import {FactPage} from "./components/FactPage";
+import {LoginPage} from "./components/LoginPage";
+import {Page404} from "./components/Page404";
+import {AuthorizedPage} from "./components/AuthorizedPage";
+
+export const user = {
+    login: false
+};
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <header>
+                <p><NavLink to="/" className={({isActive}) => isActive ? 'active' : 'pending'}>Главная</NavLink></p>
+                <p><NavLink to="/login" className={({isActive}) => isActive ? 'active' : 'pending'}>Войти</NavLink></p>
+                <p><NavLink to="/autho" className={({isActive}) => isActive ? 'active' : 'pending'}>Интересный факт</NavLink></p>
+            </header>
+            <Routes>
+                <Route path="/"
+                       element={<ContainerFacts/>}/>
+                <Route path="/fact/:id"
+                       element={<FactPage/>}/>
+                <Route path="/login"
+                       element={<LoginPage/>}/>
+                <Route path="/autho"
+                       element={<AuthorizedPage/>}/>
+                <Route path='*'
+                       element={<Page404/>}/>
+            </Routes>
+        </div>
+    );
 }
 
 export default App;
